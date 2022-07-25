f9226509bdb77f339ce472243a70ffdec7193ba0
dluca22 <luc.due@tim.it>
2022-07-25 15:36:16 +0200

    added the txt file and opened in the app

--- analize.py
+++ analize.py
@@ -6,5 +6,9 @@
 
 # this should be an helper function, as it sould work independently from the source of the data (in this case indeed, but later should manage the same text from other websites/listing)
 
+# TO BE REMOVED - just testing read from an example file with a random listing text
+# file I/O is built-in in python
+with open("random_listing.txt", 'r') as descr:
+    # tried to count the words in the file and it works
 
-dummy_listing = ''
+    # to do
