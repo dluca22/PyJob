cb8c54964b875bba4c7cb9641da111130733f3b0
dluca22 <luc.due@tim.it>
2022-07-26 13:03:47 +0200

    loaded the list as a dict with default value

--- testing.py
+++ testing.py
@@ -2,15 +2,15 @@ import json
 import csv
 
 dict = {}
-
+# saved raw input from github because no internet connection, so saved to a .txt file and continued offline
 with open('raw_list_from_github.txt', 'r') as raw:
+    #reads as a string, removing quotes from each str
     reader = raw.read().replace("'", "")
+    # transforms the str obj to a list separated at the ","
     list = list(reader.split(","))
-    # csv_reader = csv.reader(raw)
-    # print(reader[8])
-    # print(csv_reader)
 
-    # for item in reader:
-    #     dict[item] = 0
-    # print(dict)
-    for i in list
+    # loop the list and gives def value of 0
+    for i in list:
+        dict[i] = 0
+
+    print(dict)
