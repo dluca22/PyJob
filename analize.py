cd35f89e7bc05a1206b1b397ccfba38674dd2af9
dluca22 <luc.due@tim.it>
2022-07-29 13:01:11 +0200

    fixed the count for each keyord ti +1 per each text, added output file

--- analize.py
+++ analize.py
@@ -8,13 +8,15 @@ from dictionary import languages
 # this should be an helper function, as it sould work independently from the source of the data (in this case indeed, but later should manage the same text from other websites/listing)
 
 dict = {
-    'css' : 0,
-    'javascript' : 0,
-    'android' : 0
+    'css': 0,
+    'javascript': 0,
+    'android': 0
 }
+# second dictionary with only the non zero values
+results = {}
+
 # "c++" throws an error, had to change to c+
 # languages is a dict imported from another python file
-
 
 
 # TO BE REMOVED - just testing read from an example file with a random listing text
@@ -28,14 +30,18 @@ with open("random_listing2.txt", 'r') as descr:
     # looping over keys in the dict, it assigns the number of times the str gets appended to the matches[] list
     for key in languages:
         matches = re.findall(rf'\b{key}\b', text)
-        languages[key] = len(matches)
+        # if matches creates a list with
+        if len(matches) >= 1:
+            # just add 1 to the dict
+            languages[key] = +1
+    with open('results.txt', 'a'):
 
-    # print only the keys that are not 0
-    for k, v in languages.items():
-        if v != 0:
-            print(k,":", v)
+        # print only the keys that are not 0
+        for k, v in languages.items():
+            if v != 0:
+                # print(k, ":", v)
+                results.keys() = languages[k]
+                results.values() = languages[v]
 
-
-
-            # TODO only add a +1 value TO the requirement/skill for each listing
-            # one single job offer doesn't have to give like 5 points to "PHP"
+        print(results)
+        
