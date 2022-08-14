8da8567c7acab6379c5fea76ea22559fe949f367
dluca22 <luc.due@tim.it>
2022-08-14 13:20:16 +0200

    started first page

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-from flask import Flask
+from distutils.log import debug
+from flask import Flask, render_template
 
-Flask = 
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+@app.route("/index")
+def index():
+
+    return render_template("home.html")
+
+@app.route("/start")
+def start():
+
+    return render_template("start.html")
+    ''
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
