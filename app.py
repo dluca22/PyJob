3191c1dfbe61f56f61ea1ccb59a1ec540a72f622
dluca22 <luc.due@tim.it>
2022-08-15 03:44:43 +0200

    aggiunto bootstrap, TODO, auto update

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 from distutils.log import debug
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
+
 
 @app.route("/")
 @app.route("/home")
 @app.route("/index")
 def index():
+    list = []
+    with open("list_of_keys.txt", "r") as file:
 
-    return render_template("index.html")
+        for line in file:
+            list.append(line)
+            
+    return render_template("index.html", list=list)
 
-@app.route("/test")
-def test():
+# @app.route("/test")
+# def test():
 
-    return render_template("test.html", visibility="hidden")
-    ''
+#     return render_template("test.html", visibility="hidden")
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
+
+    # TEMP - reload every secondss
+    while True:
+        sleep(1)
+        app.run()
